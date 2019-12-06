3a7e89251f263166af1a9259765e84eb1d147616
vmhernandes <vmh@icomp.ufam.edu.br>
2019-12-05 23:47:40 -0400

    migration que faltava criada usando o framework...

--- laravel/database/migrations/2019_12_06_032033_create_resposta_responde_reclamacao.php
+++ laravel/database/migrations/2019_12_06_032033_create_resposta_responde_reclamacao.php
@@ -14,23 +14,23 @@ class CreateRespostaRespondeReclamacao extends Migration
     public function up()
     {
         Schema::create('resposta_responde_reclamacao', function (Blueprint $table) {
+            $table->integer('id_reclamacao')->unsigned()->length(10);
             $table->integer('id_resposta')->unsigned()->length(10);
 
-            $table->integer('id_reclamacao')->unsigned()->length(10);
 
-            $table->primary(array('id_resposta', 'id_reclamacao'));
-
-            $table->foreign('id_resposta')
-                ->references('id_resposta')
-                ->on('resposta')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
+            $table->primary(array('id_reclamacao','id_resposta'));
 
             $table->foreign('id_reclamacao')
                 ->references('id_reclamacao')
                 ->on('reclamacao')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
+
+            $table->foreign('id_resposta')
+                ->references('id_resposta')
+                ->on('resposta')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');    
         });
     }
 
