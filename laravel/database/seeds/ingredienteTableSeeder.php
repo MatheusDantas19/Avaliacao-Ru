5c988cd242f2533a308353cbda22cf0843fcbd2a
Tanizia <tdlf@icomp.ufam.edu.br>
2019-12-15 17:31:09 -0400

    Sedeer Ingredientes

--- laravel/database/seeds/ingredienteTableSeeder.php
+++ laravel/database/seeds/ingredienteTableSeeder.php
@@ -12,5 +12,49 @@ class ingredienteTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('ingrediente')->insert(
+            [
+                [
+                    'nome' => 'Sal',
+                    'descricao' => 'Sal Branco Comum'
+                ],
+                [
+                    'nome' => 'Tomate',
+                    'descricao' => 'Tomate maduro'
+                ],
+                [
+                    'nome' => 'Extrato de Tomate',
+                    'descricao' => 'Molho de Tomate para macarrão e carnes'
+                ],
+                [
+                    'nome' => 'Açucar',
+                    'descricao' => 'Açucar Branco Comum'
+                ],
+                [
+                    'nome' => 'Macarão',
+                    'descricao' => 'Massa de macarão'
+                ],
+                [
+                    'nome' => 'Arroz',
+                    'descricao' => 'Arroz Branco Integral'
+                ],
+                [
+                    'nome' => 'Osso Buco',
+                    'descricao' => 'carne Bovina para cozinhar'
+                ],
+                [
+                    'nome' => 'Ovo',
+                    'descricao' => 'Ovo de galinha'
+                ],
+                [
+                    'nome' => 'Oléo',
+                    'descricao' => 'Oléo de Soja'
+                ],
+                [
+                    'nome' => 'Feijão Carioca',
+                    'descricao' => ''
+                ]
+            ]
+        );
     }
 }
