0ec47b91ef2351bd8722eb956f557fec9564788e
vmhernandes <vmh@icomp.ufam.edu.br>
2019-12-17 06:50:24 -0400

    create resposta concluido

--- laravel/app/Http/Controllers/RespostaController.php
+++ laravel/app/Http/Controllers/RespostaController.php
@@ -13,15 +13,77 @@ class RespostaController extends Controller
 {
     public function createGet(Request $request){
         if ($request->session()->exists('loginAdmin')) {
+            $mensagem = $request->session()->get('mensagem');
             $id_admin = $request->session()->get('id_admin');
-            $reclamacoes = DB::table('admin_gerencia_restaurante')
-            ->join('restaurante','admin_gerencia_restaurante.id_restaurante','=','restaurante.id_restaurante')
-            ->join('reclamacao_denuncia_res','restaurante.id_restaurante','=','reclamacao_denuncia_res.id_restaurante')
-            ->join('reclamacao','reclamacao_denuncia_res.id_reclamacao','=','reclamacao.id_reclamacao')
-            ->where('admin_gerencia_restaurante.id_admin',$id_admin)->get();
-            return view("resposta.create", compact('reclamacoes'));
+            $reclamacoes = DB::select(
+                'SELECT reclamacao.id_reclamacao, reclamacao.categoria, reclamacao.descricao, reclamacao.data_ocorrencia,
+            aluno_abre_reclamacao.data,
+            reclamacao_denuncia_res.id_restaurante,
+            restaurante.campus, restaurante.setor, restaurante.local,
+            prato.id_prato, prato.descricao as elaboracao
+
+            FROM reclamacao
+                JOIN aluno_abre_reclamacao on aluno_abre_reclamacao.id_reclamacao = reclamacao.id_reclamacao
+                JOIN reclamacao_denuncia_res on reclamacao_denuncia_res.id_reclamacao = reclamacao.id_reclamacao
+                JOIN admin_gerencia_restaurante on admin_gerencia_restaurante.id_restaurante = reclamacao_denuncia_res.id_restaurante
+                JOIN restaurante on restaurante.id_restaurante = reclamacao_denuncia_res.id_restaurante
+                LEFT JOIN reclamacao_cita_prato on reclamacao_cita_prato.id_reclamacao = reclamacao.id_reclamacao
+                LEFT JOIN prato on prato.id_prato = reclamacao_cita_prato.id_prato
+            WHERE reclamacao.id_reclamacao in (
+                    SELECT reclamacao.id_reclamacao
+				    FROM reclamacao
+					    LEFT JOIN resposta_responde_reclamacao on reclamacao.id_reclamacao = resposta_responde_reclamacao.id_reclamacao
+					    LEFT JOIN resposta on resposta_responde_reclamacao.id_resposta = resposta.id_resposta
+				    WHERE resposta_responde_reclamacao.id_resposta IS NULL)
+                AND admin_gerencia_restaurante.id_admin = '.$id_admin.';',[1]);
+
+
+
+            return view("resposta.create", compact(['reclamacoes','mensagem']));
         } else {
             return redirect('/loginAdmin');
         }
+    }
+
+    public function createPost(Request $request){
+        if ($request->session()->exists('loginAdmin')) {
+            $id_admin = $request->session()->get('id_admin');
+            $id_reclamacao = $request->id_reclamacao;
+            $resposta = $request->resposta;
+            $teste = DB::table('resposta_responde_reclamacao')
+                        ->where('resposta_responde_reclamacao.id_reclamacao','=',$id_reclamacao)->get();
+            if(count($teste) > 0){
+                $mensagem = $request->session()->flash('mensagem', $teste);
+                return redirect("/createResposta");
+            }
+            else{
+                $teste2 = DB::select(
+                    'SELECT *
+                    FROM reclamacao
+                        JOIN reclamacao_denuncia_res on reclamacao.id_reclamacao = reclamacao_denuncia_res.id_reclamacao
+                        JOIN admin_gerencia_restaurante on admin_gerencia_restaurante.id_restaurante = reclamacao_denuncia_res.id_restaurante
+                        LEFT JOIN resposta_responde_reclamacao on reclamacao.id_reclamacao = resposta_responde_reclamacao.id_reclamacao
+                        LEFT JOIN resposta on resposta_responde_reclamacao.id_resposta = resposta.id_resposta
+                    WHERE admin_gerencia_restaurante.id_admin = '.$id_admin.' and resposta_responde_reclamacao.id_resposta IS NULL
+                            and reclamacao.id_reclamacao = '.$id_reclamacao.'', [1]);
+                if(count($teste2) > 0){
+                    $resposta_id = DB::table('resposta')->insertGetId(['status'=>1,'descricao'=>$resposta]);
+
+                    DB::table('admin_elabora_resposta')
+                        ->insert(['id_admin'=>$id_admin,'id_resposta'=>$resposta_id,'data'=> new DateTime()]);
+
+                    DB::table('resposta_responde_reclamacao')
+                        ->insert(['id_reclamacao'=>$id_reclamacao,'id_resposta'=>$resposta_id]);
+                    $mensagem = $request->session()->flash('mensagem', count($teste2));
+                    return redirect("/createResposta");
+
+                } else{
+                    $mensagem = $request->session()->flash('mensagem', 'Você não tem permissao para responder essa reclamação, verifique seus dados');
+                    return redirect("/createResposta");
+                }
+            }
+        } else {
+        return redirect('/loginAdmin');
+    }
     }
 }
--- laravel/resources/views/resposta/create.blade.php
+++ laravel/resources/views/resposta/create.blade.php
@@ -3,27 +3,65 @@
     Responder Reclamação
 @endsection
 @section('conteudo')
-{{$reclamacoes}}
+<table>
+    <thead>
+        <caption>Reclamações de seus Restaurantes:</caption>
+        <tr>
+            <th>id_reclamação</th>
+            <th>categoria</th>
+            <th>id_restaurante</th>
+            <th>campus</th>
+            <th>setor</th>
+            <th>local</th>
+            <th>dia ocorrência</th>
+            <th>dia postagem</th>
+            <th>descricao da ocorrencia</th>
+            <th>id_prato</th>
+            <th>descricao do prato</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            foreach ($reclamacoes as $i => $value) {
+                echo '<tr>';
+                echo'<td>'.$value->id_reclamacao.'</td>';
+                echo'<td>'.$value->categoria.'</td>';
+                echo'<td>'.$value->id_restaurante.'</td>';
+                echo'<td>'.$value->campus.'</td>';
+                echo'<td>'.$value->setor.'</td>';
+                echo'<td>'.$value->local.'</td>';
+                echo'<td>'.$value->data_ocorrencia.'</td>';
+                echo'<td>'.$value->data.'</td>';
+                echo'<td>'.$value->descricao.'</td>';
+                echo'<td>'.$value->id_prato.'</td>';
+                echo'<td>'.$value->elaboracao.'</td>';
+                echo'</tr>';
+            }
+        @endphp
+    </tbody>
+</table>
+{{$mensagem}}
 <div class="row">
     <form class="col s12" method="POST">
         @csrf
         <div class="row">
             <div class="input-field col s6">
-                <input id="nome" name="nome" type="text" class="validate"
-                       placeholder="teste">
-                <label for="nome">Nome:</label>
+                <input id="id_reclamacao" name="id_reclamacao" type="text" class="validate"
+                       placeholder="0">
+                <label for="id_reclamacao">Id reclamacao:</label>
             </div>
         </div>
 
             <div class="row">
                 <div class="input-field col s6">
-                    <input id="senha" name="senha" type="password" class="validate"
-                           placeholder="****">
-                    <label for="senha">Senha:</label>
+                    <input id="resposta" name="resposta" type="text" class="validate"
+                           placeholder="Resposta">
+                    <label for="Resposta">Resposta:</label>
                 </div>
             </div>
 
-            <button class="btn waves-effect waves-light" type="submit">Cadastrar
+            <button class="btn waves-effect waves-light" type="submit">Responder
                 <i class="material-icons right">send</i>
             </button>
         </div>
